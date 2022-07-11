3c3d3996499bb132d7cefcb2b6c1942d4a47dae9
Sergey Podkolzin <power16bit@gmail.com>
2022-07-11 13:41:02 +0200

    feat: add Popup class

--- utilits/constants.js
+++ utilits/constants.js
@@ -24,3 +24,13 @@ export const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
+
+  //validation selectors
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup_error_visible'
+};
