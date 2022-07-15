bd72c78c8216718b458abfe0763e9ad089196c38
Sergey Podkolzin <power16bit@gmail.com>
2022-07-15 10:09:38 +0200

    feat: webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-//import './index.css';
+import './index.css';
 
 import Card  from '../components/Card.js'
 import FormValidator  from '../components/FormValidation.js';
@@ -14,7 +14,7 @@ import {
     popupEdit,
     popupAddCard,
     addButton
- } from '../utilits/constants.js';
+ } from '../../utilits/constants.js';
 
 //fullscreen function 
 function handleCardClick(name, link) { 
